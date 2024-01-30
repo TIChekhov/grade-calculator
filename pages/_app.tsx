5d1211395990ac02ce1bd85a8099c2352fa6a21f
I-Atlas <54947380+I-Atlas@users.noreply.github.com>
2024-01-30 18:15:49 +0300

    seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,21 +10,26 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Калькулятор баллов ЕГЭ</title>
+        <title>Калькулятор ЕГЭ</title>
+        <meta name="description" content="Приемная комиссия ТГПИ" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <meta
+          name="keywords"
+          content="калькулятор егэ, егэ, тгпи, чеховский институт, иститут чехова, чехов, таганрог, таганрогский институт чехова, ринх филиал"
+        />
         <meta name="msapplication-TileColor" content="#228be6" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://cars.carsan.com/" />
-        <meta name="twitter:title" content="Калькулятор баллов ЕГЭ" />
         <meta
-          name="twitter:description"
-          content="All-in-One car sharing service"
+          name="twitter:url"
+          content="https://grade-calculator-woad.vercel.app/"
         />
-        <meta name="twitter:image" content="" />
+        <meta name="twitter:title" content="Калькулятор ЕГЭ" />
+        <meta name="twitter:description" content="Приемная комиссия ТГПИ" />
+        <meta name="twitter:image" content="/twitter-image.png" />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <MantineProvider defaultColorScheme="auto">
